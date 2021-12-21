a31c16ca0958b776beb828d2e873afab1270e6a1
krotOv-kiryan <kirya.krotov.03@mail.ru>
2021-12-21 18:51:50 +1000

    s5

--- DosTest3/Controllers/HomeController.cs
+++ DosTest3/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace DosTest3.Controllers
 {
     public class HomeController : Controller
     {
+        FileModel fileModel;
         ApplicationContext _context;
         IWebHostEnvironment _appEnvironment;
 
@@ -59,7 +60,7 @@ namespace DosTest3.Controllers
                 _context.SaveChanges();
             }
 
-            return RedirectToAction("Index"); //
+            return RedirectToAction("Index"); 
         }
     
         public IActionResult Privacy()
@@ -73,59 +74,147 @@ namespace DosTest3.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        public string Coding(string file, string typeCoding)
+        {
+            return file;
+          //file typeCoding
+        }
 
         public string Win1251ToUTF8(string source)
         {
-
             string text = "";
+
+            using (StreamReader reader = new StreamReader("file.txt", Encoding.GetEncoding(1251) ))
+            using (StreamWriter writer = new StreamWriter("file1.txt", false, Encoding.UTF8) )
+            {
+               
+            }
+
             Encoding utf8 = Encoding.GetEncoding("UTF-8");
             Encoding win1251 = Encoding.GetEncoding("Windows-1251");
 
             byte[] utf8Bytes = win1251.GetBytes(text);
             byte[] win1251Bytes = Encoding.Convert(utf8, win1251, utf8Bytes);
             source = win1251.GetString(win1251Bytes);
+
             return source;
+            //  
+           
+            
         }
 
         public string Win1251ToKoi8(string source)
         {
+            string text = "";
+            Encoding koi8 = Encoding.GetEncoding("KOI-8");
+            Encoding win1251 = Encoding.GetEncoding("Windows-1251");
+
+            byte[] koi8Bytes = koi8.GetBytes(text);
+            byte[] win1251Bytes = Encoding.Convert(koi8, win1251, koi8Bytes);
+            source = win1251.GetString(win1251Bytes);
+
             return source;
         }
 
         public string Win1251ToCP886(string source)
         {
+            string text = "";
+            Encoding cp886 = Encoding.GetEncoding("CP-886");
+            Encoding win1251 = Encoding.GetEncoding("Windows-1251");
+
+            byte[] cp886Bytes = cp886.GetBytes(text);
+            byte[] win1251Bytes = Encoding.Convert(cp886, win1251, cp886Bytes);
+            source = win1251.GetString(win1251Bytes);
+
             return source;
         }
-        /*public string Win1251ToUTF8(string source)
-        {
-         return source;
-        }*/
+       
         public string Koi8ToWin1251(string source)
         {
+            string text = "";
+            Encoding win1251 = Encoding.GetEncoding("Windows-1251");
+            Encoding koi8 = Encoding.GetEncoding("KOI-8");
+
+            byte[] koi8Bytes = koi8.GetBytes(text);
+            byte[] win1251Bytes = Encoding.Convert(koi8, win1251, koi8Bytes);
+            source = win1251.GetString(win1251Bytes);
+
             return source;
         }
         public string Koi8ToCP886(string source)
         {
+            string text = "";
+            Encoding cp886 = Encoding.GetEncoding("CP-886");
+            Encoding koi8 = Encoding.GetEncoding("KOI-8");
+
+            byte[] cp886Bytes = cp886.GetBytes(text);
+            byte[] koi8Bytes = Encoding.Convert(koi8, koi8, cp886Bytes);
+            source = koi8.GetString(koi8Bytes);
+
             return source;
         }
-        /*public string Koi8ToToUTF8(string source)
+        public string Koi8ToToUTF8(string source)
         {
-         return source;
-        }*/
+            string text = "";
+            Encoding utf8 = Encoding.GetEncoding("UTF-8");
+            Encoding koi8 = Encoding.GetEncoding("KOI-8");
+
+            byte[] koi8Bytes = koi8.GetBytes(text);
+            byte[] utf8Bytes = Encoding.Convert(koi8, utf8, koi8Bytes);
+            source = utf8.GetString(utf8Bytes);
+
+            return source;
+        }
 
         public string CP886ToKoi8(string source)
         {
+            string text = "";
+            Encoding cp886 = Encoding.GetEncoding("CP-886");
+            Encoding koi8 = Encoding.GetEncoding("KOI-8");
+           
+            byte[] koi8Bytes = koi8.GetBytes(text);
+            byte[] cp886Bytes = Encoding.Convert(koi8, cp886, koi8Bytes);
+            source = cp886.GetString(cp886Bytes);
+
             return source;
         }
         public string CP886ToWin1251(string source)
         {
+            string text = "";
+            Encoding cp886 = Encoding.GetEncoding("CP-886");
+            Encoding win1251 = Encoding.GetEncoding("Windows-1251");
+
+            byte[] cp886Bytes = cp886.GetBytes(text);
+            byte[] win1251Bytes = Encoding.Convert(cp886, win1251, cp886Bytes);
+            source = win1251.GetString(win1251Bytes);
+
             return source;
         }
-        /*public string CP886ToKoi8(string source)
+        
+        public string CP886ToUTF8(string source)
         {
-         return source;
-        }*/
+            string text = "";
+            Encoding utf8 = Encoding.GetEncoding("UTF-8");
+            Encoding cp886 = Encoding.GetEncoding("CP-886");
 
-       
+            byte[] cp886Bytes = cp886.GetBytes(text);
+            byte[] utf8Bytes = Encoding.Convert(cp886, utf8, cp886Bytes);
+            source = utf8.GetString(utf8Bytes);
+
+            return source;
+        }
+
+        public string GetFile(path, name)
+        {
+
+            Process myProcess = new Process();
+            myProcess.StartInfo.FileName = cmd.exe;
+            myProcess.StartInfo.Arguments = @"/C cd " + Application.StartupPath + "/server/login & start.bat";
+            myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            myProcess.StartInfo.CreateNoWindow = true;
+            myProcess.Start();
+            return path;
+
+        }
     }
 }
